52600b0c36186752d93512f58ffa94308fcc038f
Diogo L Scarmagnani <diogols@live.com>
2023-05-15 20:42:56 -0300

    separando por tipos

--- App/Models/DAO/PokemonDAO.php
+++ App/Models/DAO/PokemonDAO.php
@@ -14,10 +14,19 @@ class PokemonDAO extends BaseDAO
 
             return $resultado->fetchObject(Pokemon::class);
         } else {
-            // $resultado = $this->select("SELECT * FROM pokemon");
-            $resultado = $this->select("SELECT P.id, P.name, T.type_name FROM pokemon P
-            INNER JOIN pokemons_types PT ON P.id = PT.pokemon_id
-            INNER JOIN type T ON PT.type_id = T.id LIMIT 50");
+            // $sql = "SELECT * FROM pokemon";
+            $sql = "SELECT
+                        P.id
+                        ,P.name
+                        # ,T.type_name
+                        ,GROUP_CONCAT(T.type_name ORDER BY P.id SEPARATOR ' | ') AS type_name
+                    FROM pokemon P
+                    INNER JOIN pokemons_types PT ON P.id = PT.pokemon_id
+                    INNER JOIN type T ON PT.type_id = T.id
+                    GROUP BY id, name
+                    LIMIT 50";
+            
+            $resultado = $this->select($sql);
             
             return $resultado->fetchAll(\PDO::FETCH_CLASS, pokemon::class);
         }
